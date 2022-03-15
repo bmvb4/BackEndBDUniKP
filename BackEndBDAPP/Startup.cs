7b22ea8d022408027dbea61c88732d4bb8a93917
KirilStoyanov <kiril.stoyanov.s@gmail.com>
2022-03-15 16:15:30 +0200

    sad1

--- BackEndBDAPP/Startup.cs
+++ BackEndBDAPP/Startup.cs
@@ -75,7 +75,7 @@ namespace BackEndBDAPP
             {
                 app.UseDeveloperExceptionPage();
             }
-            app.UseCors(x => x.WithOrigins("http://localhost:4200", "http://localhost:4200/register")
+            app.UseCors(x => x.WithOrigins("http://localhost:4200")
             .AllowAnyMethod()
             .AllowAnyHeader());
             app.UseHttpsRedirection();
