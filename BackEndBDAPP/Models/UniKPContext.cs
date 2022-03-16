3a512bae6d68512add45d4ee6b980f9dc6db6f62
KirilStoyanov <kiril.stoyanov.s@gmail.com>
2022-03-16 11:49:32 +0200

    stop Email
    Update for nginx

--- BackEndBDAPP/Models/UniKPContext.cs
+++ BackEndBDAPP/Models/UniKPContext.cs
@@ -28,11 +28,7 @@ namespace BackEndBDAPP.Models
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=159.65.94.122;User ID=sa;Password=beLeaf1999uni;Database=UniKP;MultipleActiveResultSets=False;Encrypt=False;TrustServerCertificate=True;Connection Timeout=30;");
-            }
+            
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -190,7 +186,7 @@ namespace BackEndBDAPP.Models
             modelBuilder.Entity<Tag>(entity =>
             {
                 entity.HasKey(e => e.TagName)
-                    .HasName("PK__Tag__BDE0FD1CE51864E9");
+                    .HasName("PK__Tag__BDE0FD1C2FEA6757");
 
                 entity.ToTable("Tag");
 
