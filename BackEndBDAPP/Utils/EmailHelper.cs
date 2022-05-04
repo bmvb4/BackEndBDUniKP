baa628e091c93ee91516d29ce98144899cc872cc
KirilStoyanov <kiril.stoyanov.s@gmail.com>
2022-05-04 13:39:03 +0300

    Email link Update

--- BackEndBDAPP/Utils/EmailHelper.cs
+++ BackEndBDAPP/Utils/EmailHelper.cs
@@ -16,7 +16,7 @@ namespace BackEndBDAPP.Utils
 
             mailMessage.Subject = "Confirm your email";
             mailMessage.IsBodyHtml = true;
-            mailMessage.Body = "https://beleaf.me/Email?Username="+Username+"&Code="+confirmationLink;
+            mailMessage.Body = "https://beleaf.me/Email/"+Username+"/"+confirmationLink;
 
             SmtpClient client = new SmtpClient();
             client.EnableSsl = true;
